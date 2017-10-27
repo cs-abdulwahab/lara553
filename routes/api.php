207c13d9efd32608cbaa2dbbbc088d1e52ec4e1f
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-10-27 18:38:38 +0500

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Http\Controllers\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    Auth::guard('api')->user();
+
+    return Auth::guard('api')->user();
+});
+
+
+// Route::post('/register', 'UserController@authenticate');
+///*Route::post('register', 'UserController@register');
+
+//Route::post('login','Auth\UserLoginController@login');
+//Route::post('logout', 'Auth\LoginController@logout');*/
+
+
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function () {
+    //    Route::resource('task', 'TasksController');
+    Route::apiResource('hostel', 'HostelController');
+   // Route::apiResource('faculty', 'FacultyController');
+
+    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
+    #adminlte_api_routes
+});
+/*
+Route::apiResource('survey', 'SurveyController');
+Route::apiResource('survey.question', 'QuestionController');
+
+
+Route::apiResource('resturant', 'RestaurantController');
+Route::apiResource('mymodel', 'MyModelController');
+
+
+
+
+*/
